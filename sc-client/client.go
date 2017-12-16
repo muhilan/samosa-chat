9c6ef2aa19ba6c2f8966237d8957b252b59097a2
rohlx <project@rohlx.com>
2017-12-16 12:28:50 -0700

    try libui

--- sc-client/client.go
+++ sc-client/client.go
@@ -1,70 +1,31 @@
 package main
 
 import (
-    "log"
-    "runtime"
-
-    "github.com/go-gl/gl/v4.1-core/gl" // OR: github.com/go-gl/gl/v2.1/gl
-    "github.com/go-gl/glfw/v3.2/glfw"
-)
-
-const (
-    width  = 500
-    height = 500
+    "github.com/andlabs/ui"
 )
 
 func main() {
-    runtime.LockOSThread()
-
-    window := initGlfw()
-    defer glfw.Terminate()
-
-     program := initOpenGL()
-
-
-    for !window.ShouldClose() {
-        draw(window, program)
-    }
-}
-
-// initGlfw initializes glfw and returns a Window to use.
-func initGlfw() *glfw.Window {
-    if err := glfw.Init(); err != nil {
-            panic(err)
-    }
-    
-    glfw.WindowHint(glfw.Resizable, glfw.False)
-    glfw.WindowHint(glfw.ContextVersionMajor, 4) // OR 2
-    glfw.WindowHint(glfw.ContextVersionMinor, 1)
-    glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-    glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
-
-    window, err := glfw.CreateWindow(width, height, "Samosa chat", nil, nil)
+    err := ui.Main(func() {
+        chatTxt := ui.NewEntry()
+        button := ui.NewButton("Send")
+        greeting := ui.NewLabel("")
+        box := ui.NewVerticalBox()
+        // box.Append(ui.NewLabel("Enter your name:"), false)
+        box.Append(chatTxt, false)
+        box.Append(button, false)
+        box.Append(greeting, false)
+        window := ui.NewWindow("Samosa-chat", 200, 100, false)
+        window.SetChild(box)
+        button.OnClicked(func(*ui.Button) {
+            greeting.SetText("\n" + chatTxt.Text() )
+        })
+        window.OnClosing(func(*ui.Window) bool {
+            ui.Quit()
+            return true
+        })
+        window.Show()
+    })
     if err != nil {
-            panic(err)
+        panic(err)
     }
-    window.MakeContextCurrent()
-
-    return window
-}
-
-// initOpenGL initializes OpenGL and returns an intiialized program.
-func initOpenGL() uint32 {
-    if err := gl.Init(); err != nil {
-            panic(err)
-    }
-    version := gl.GoStr(gl.GetString(gl.VERSION))
-    log.Println("OpenGL version", version)
-
-    prog := gl.CreateProgram()
-    gl.LinkProgram(prog)
-    return prog
-}
-
-func draw(window *glfw.Window, program uint32) {
-    gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-    gl.UseProgram(program)
-    
-    glfw.PollEvents()
-    window.SwapBuffers()
 }
