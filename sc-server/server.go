ba02f4ab65ffc99b10b8a10e623b1016779b8bc1
rohlx <project@rohlx.com>
2017-12-15 21:55:30 -0700

    Add server

--- sc-server/server.go
+++ sc-server/server.go
@@ -1,9 +1,38 @@
 package main
 
-import "fmt"
+import ( "net/http"
+ 
+   "golang.org/x/net/http2"
+
+   "fmt"
+
+   )
 
 func main(){
 
-fmt.Println("Server")
+	fmt.Println("Server")
 
+	var srv http.Server
+
+	srv.Addr = ":8081"
+
+	
+
+	//Enable http2
+
+	http2.ConfigureServer(&srv, nil)
+		http.HandleFunc("/", handlerHtml)
+	fmt.Println("Server started on 8081 ")
+	srv.ListenAndServeTLS("certs/localhost.cert", "certs/localhost.key")
+
+	
+
+
+
+
+}
+
+
+func handlerHtml(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1><center> Hello from Go! </h1></center>"))
 }
