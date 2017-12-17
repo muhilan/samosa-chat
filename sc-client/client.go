bbd841f1f496e955e292439bee32fb6ef721a9b2
rohlx <project@rohlx.com>
2017-12-16 19:40:35 -0700

    Update client and server

--- sc-client/client.go
+++ sc-client/client.go
@@ -1,23 +1,46 @@
 package main
 
 import (
-    "github.com/andlabs/ui"
+    // "github.com/andlabs/ui"
+    "log"
+    "github.com/ProtonMail/ui"
+    "time"
+    "net/http"
+    "fmt"
+    "crypto/tls"
+    "io/ioutil"
+    "bytes"
+    "encoding/json"
 )
 
+type Message struct {
+    Owner   string  
+    Time string 
+    Text string 
+}
+
+type MessageContext struct {
+    msgs []Message  `json:"msgs"`
+}
+
 func main() {
+    log.Print("Entry")
     err := ui.Main(func() {
-        chatTxt := ui.NewEntry()
+        newchat := ui.NewEntry()
         button := ui.NewButton("Send")
-        greeting := ui.NewLabel("")
+        multi := ui.NewMultilineNonWrappingEntry()
+        multi.ReadOnly()
         box := ui.NewVerticalBox()
-        // box.Append(ui.NewLabel("Enter your name:"), false)
-        box.Append(chatTxt, false)
-        box.Append(button, false)
-        box.Append(greeting, false)
-        window := ui.NewWindow("Samosa-chat", 200, 100, false)
+        horbox := ui.NewHorizontalBox()
+        horbox.Append(newchat, true)
+        horbox.Append(button, true)
+        box.Append(multi,true)
+        box.Append(horbox, false)
+       
+        window := ui.NewWindow("Samosa Chat", 300, 600, false)
         window.SetChild(box)
-        button.OnClicked(func(*ui.Button) {
-            greeting.SetText("\n" + chatTxt.Text() )
+        button.OnClicked(func (*ui.Button) {
+            multi.Append(formatText(newchat.Text()))
         })
         window.OnClosing(func(*ui.Window) bool {
             ui.Quit()
@@ -29,3 +52,38 @@ func main() {
         panic(err)
     }
 }
+
+func formatText(newchat string) string{
+    str := string(time.Now().Format("2006-01-02 15:04:05")) + "  " + newchat + "\n"
+    Post(str)
+    return  str
+}
+
+func Post(str string){
+
+    var msg = &Message { Owner: "mgm", Time : time.Now().Format("2006-01-02 15:04:05"), Text: str}
+    jsonStr, err := json.Marshal(msg)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
+    tr := &http.Transport{
+        TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+    }
+    client := &http.Client{Transport: tr}
+    r, _ := http.NewRequest("POST", "https://localhost:8080", bytes.NewBuffer(jsonStr)) // <-- URL-encoded payload
+    r.Header.Add("Content-Type", "application/json")
+
+    resp, err := client.Do(r)
+    if err!=nil {
+        log.Print(err)
+    } else {
+        fmt.Println(resp.Status)
+        data, _ := ioutil.ReadAll(resp.Body)
+        fmt.Println(string(data))
+        resp.Body.Close()
+    }
+
+}
+
--- sc-server/server.go
+++ sc-server/server.go
@@ -1,10 +1,19 @@
 package main
 
 import ( "net/http"
-   "golang.org/x/net/http2"
+   // "golang.org/x/net/http2"
    "fmt"
    "log"
+    "encoding/json"
+    "io/ioutil"
    )
+
+type Message struct {
+	Owner   string  `json:"Owner"`
+	Time string `json:"Time"`
+	Text string `json:"Text"`
+}
+
 
 func main(){
 
@@ -12,8 +21,8 @@ func main(){
 
 	srv.Addr = ":8080"
 
-	http2.ConfigureServer(&srv, nil)
-	http.HandleFunc("/", handlerHtml)
+	// http2.ConfigureServer(&srv, nil)
+	http.HandleFunc("/", handler)
 	fmt.Println("Server started on 8080 ")
 	err := srv.ListenAndServeTLS("certs/localhost.cert", "certs/localhost.key")
 	log.Fatal(err)
@@ -21,6 +30,28 @@ func main(){
 }
 
 
-func handlerHtml(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1><center> Hello from Go! </h1></center>"))
+func handler(w http.ResponseWriter, r *http.Request) {
+	b, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	// Unmarshal
+	var msg Message
+	err = json.Unmarshal(b, &msg)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	output, err := json.Marshal(msg)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	w.Header().Set("content-type", "application/json")
+	w.Write(output)
 }
+
