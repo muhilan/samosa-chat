6bd138d6a78d15dfcd02187f7115fd27e2f7cbf6
Muhilan <mgmuhilan@gmail.com>
2017-12-21 00:07:25 -0700

    Use tls dial instead of net/http

--- sc-client/client.go
+++ sc-client/client.go
@@ -1,123 +1,155 @@
 package main
 
 import (
-    // "github.com/andlabs/ui"
-    "log"
-    "github.com/ProtonMail/ui"
-    "time"
-    "net/http"
-    "fmt"
-    "crypto/tls"
-    "io/ioutil"
-    "bytes"
-    "encoding/json"
-    "os/user"
+	// "github.com/andlabs/ui"
+	//"bytes"
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"github.com/ProtonMail/ui"
+	"io/ioutil"
+	"log"
+	//"net/http"
+	"os/user"
+	"time"
+	"bufio"
 )
-type ClientMetadata struct{
-    Owner string
-    ChatServerHost string
-    ChatServerPort string
-    OwnerEmail string
-}
-type Message struct {
-    Owner   string  
-    Time string 
-    Text string 
+
+type ClientMetadata struct {
+	Owner          string
+	ChatServerHost string
+	ChatServerPort string
+	OwnerEmail     string
 }
 
+
 type MessageContext struct {
-    msgs []Message  `json:"msgs"`
+	Text string
+	Owner string
+	Time int64
 }
 
 var clientMD ClientMetadata
 var home string
 
+var messages = make(chan MessageContext)
+var conn *tls.Conn
+
 func main() {
-    log.Print("Entry")
-    clientMD = getClientMetaData()
-    err := ui.Main(func() {
-        newchat := ui.NewEntry()
-        button := ui.NewButton("Send")
-        multi := ui.NewMultilineNonWrappingEntry()
-        multi.ReadOnly()
-        box := ui.NewVerticalBox()
-        horbox := ui.NewHorizontalBox()
-        horbox.Append(newchat, true)
-        horbox.Append(button, true)
-        box.Append(multi,true)
-        box.Append(horbox, false)
-       
-        window := ui.NewWindow("Samosa Chat", 300, 600, false)
-        window.SetChild(box)
-        button.OnClicked(func (*ui.Button) {
-            multi.Append(formatText(newchat.Text()))
-        })
-        window.OnClosing(func(*ui.Window) bool {
-            ui.Quit()
-            return true
-        })
-        window.Show()
-    })
-    if err != nil {
-        panic(err)
-    }
+	log.Print("Entry")
+
+	clientMD = getClientMetaData()
+    var err error
+	conn, err = tls.Dial("tcp", fmt.Sprintf("%s:%s", clientMD.ChatServerHost,clientMD.ChatServerPort), &tls.Config{InsecureSkipVerify : true})
+	if err != nil {
+		panic(err)
+	}
+	multi := ui.NewMultilineNonWrappingEntry()
+	multi.ReadOnly()
+
+	go func() {
+		reader := bufio.NewReader(conn)
+		for {
+			in, err := reader.ReadString('\n')
+			fmt.Printf("Received message %s\n",string(in))
+			if err != nil {
+				break
+			}
+			var msg MessageContext
+			err = json.Unmarshal([]byte(in), &msg)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			messages <- msg
+		}
+	}()
+	go func(){
+		select {
+		case msg := <-messages:
+			fmt.Println("About to paint message")
+			multi.Append(formatText(msg.Time,msg.Text))
+		}
+	}()
+
+	err = ui.Main(func() {
+		newchat := ui.NewEntry()
+		button := ui.NewButton("Send")
+
+		box := ui.NewVerticalBox()
+		horbox := ui.NewHorizontalBox()
+		horbox.Append(newchat, true)
+		horbox.Append(button, true)
+		box.Append(multi, true)
+		box.Append(horbox, false)
+
+		window := ui.NewWindow("Samosa Chat", 300, 600, false)
+		window.SetChild(box)
+		button.OnClicked(func(*ui.Button) {
+			Post(newchat.Text(), time.Now().Unix())
+		})
+		window.OnClosing(func(*ui.Window) bool {
+			ui.Quit()
+			return true
+		})
+		window.Show()
+	})
+	if err != nil {
+		panic(err)
+	}
+
+
 }
 
-func formatText(newchat string) string{
-    currentEpocy := int64(time.Now().Unix())
-    tm := time.Unix(currentEpocy, 0)
-    fmt.Println(tm)
-
-    str := fmt.Sprintf(  "%s (%s): %s \n", tm.Format("2006-01-02 15:04:05"), clientMD.Owner, newchat)
-    Post(str)
-    return  str
+func formatText(epoch int64, newchat string) string {
+	tm := time.Unix(epoch, 0)
+	str := fmt.Sprintf("%s (%s): %s \n", tm.Format("2006-01-02 15:04:05"), clientMD.Owner, newchat)
+	Post(str, epoch)
+	return str
 }
 
-func Post(str string){
-
-    var msg = &Message { Owner: "mgm", Time : time.Now().Format("2006-01-02 15:04:05"), Text: str}
-    jsonStr, err := json.Marshal(msg)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-
-    tr := &http.Transport{
-        TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-    }
-    client := &http.Client{Transport: tr}
-    r, _ := http.NewRequest("POST", "https://localhost:8080", bytes.NewBuffer(jsonStr)) // <-- URL-encoded payload
-    r.Header.Add("Content-Type", "application/json")
-
-    resp, err := client.Do(r)
-    if err!=nil {
-        log.Print(err)
-    } else {
-        fmt.Println(resp.Status)
-        data, _ := ioutil.ReadAll(resp.Body)
-        fmt.Println(string(data))
-        resp.Body.Close()
-    }
-
+func Post(str string, epoch int64) {
+	msgCtx := MessageContext{ Owner: clientMD.Owner, Time: epoch, Text: str }
+	body := postPayload(createPayload(&msgCtx))
+	fmt.Println(body)
+	fmt.Fprint(conn, body)
 }
 
 /* Helper method to retrieve the client specific json file */
 
 func getClientMetaData() ClientMetadata {
-    raw, err := ioutil.ReadFile(homeDir()+ "/.samosa-chat.json")
-    if err != nil {
-        fmt.Println(err.Error())
-    }
-    var c ClientMetadata
-    json.Unmarshal(raw, &c)
-    return c
+	raw, err := ioutil.ReadFile(homeDir() + "/.samosa-chat.json")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	var c ClientMetadata
+	json.Unmarshal(raw, &c)
+	return c
 }
 
 /* Util method to retrieve the home dir */
 func homeDir() string {
-    usr, err := user.Current()
-    if err != nil {
-        log.Fatal( err )
-    }
-    return usr.HomeDir
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return usr.HomeDir
+}
+
+func formatDate(epoch int64) string {
+    tm := time.Unix(epoch, 0)
+    return tm.Format("2006-01-02 15:04:05")
+}
+
+func postPayload(msg string) string{
+	return fmt.Sprintf("POST / HTTP/1.1\nContent-Type: application/json\nConnection: Keep-Alive\nHost: 127.0.0.1\nUser-Agent: chat-client\n\n%s\n\n",msg)
+}
+
+func createPayload(msgCtx *MessageContext) string {
+	b, err := json.Marshal(msgCtx)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return string(b)
 }
--- sc-server/server.go
+++ sc-server/server.go
@@ -1,35 +1,74 @@
 package main
 
-import ( "net/http"
-   // "golang.org/x/net/http2"
-   "fmt"
-   "log"
-    "encoding/json"
-    "io/ioutil"
-   )
+import (
+	"net/http"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+)
 
 type Message struct {
-	Owner   string  `json:"Owner"`
-	Time string `json:"Time"`
-	Text string `json:"Text"`
-
+	Owner string
+	Time  int64
+	Text  string
 }
 
+type ConnectionContext struct {
+	connection net.Conn
+	owner string
+}
+var activeConns = make(chan ConnectionContext)
+var deadConns = make(chan net.Conn,10)
+var connMap = make(map[net.Conn]string)
+var messages = make(chan Message)
 
-func main(){
+
+func main() {
 
 	var srv http.Server
 
 	srv.Addr = ":8080"
 
 	// http2.ConfigureServer(&srv, nil)
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", hiJack)
+	//http.HandleFunc("/hijack", hiJack)
 	fmt.Println("Server started on 8080 ")
+	go func() {
+		for {
+			select {
+			case connCtx := <-activeConns:
+				fmt.Printf("Handling connection for %s ", connCtx.owner)
+				_ , err := connCtx.connection.Write([]byte("Output"))
+				if err != nil {
+					deadConns <- connCtx.connection
+				}
+			case message := <-messages:
+				fmt.Printf("Received Msg : %s \n", message)
+				for conn, clientId := range connMap {
+					go func(conn net.Conn, msg string){
+						fmt.Printf("Sending message to client : %s , Message => %s \n", clientId, message.Text)
+						_,err := conn.Write([]byte(msg))
+						if err != nil {
+							deadConns <- conn
+						}
+					}(conn, message.Text)
+				}
+			case conn := <- deadConns:
+				conn.Close()
+				delete(connMap, conn)
+			}
+
+		}
+	}()
+
 	err := srv.ListenAndServeTLS("certs/localhost.cert", "certs/localhost.key")
 	log.Fatal(err)
 
-}
 
+
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	b, err := ioutil.ReadAll(r.Body)
@@ -56,3 +95,49 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	w.Write(output)
 }
 
+
+
+
+
+func hiJack(w http.ResponseWriter, r *http.Request){
+    fmt.Println("Message received")
+	b, err := ioutil.ReadAll(r.Body)
+	fmt.Println(string(b))
+	defer r.Body.Close()
+	if err != nil {
+		fmt.Println(err.Error())
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	// Unmarshal
+	var msg Message
+	err = json.Unmarshal(b, &msg)
+	if err != nil {
+		fmt.Println(err.Error())
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	hj, ok := w.(http.Hijacker)
+	if !ok {
+		http.Error(w, "web server doesn't support hijacking", http.StatusInternalServerError)
+		return
+	}
+	conn, bufrw, err := hj.Hijack()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		deadConns <- conn
+		return
+	}
+	// Don't forget to close the connection:
+	//defer conn.Close()
+	bufrw.WriteString("")
+	bufrw.Flush()
+
+	//connMap[msg.Owner] = conn
+	activeConns <- ConnectionContext{connection: conn, owner: msg.Owner}
+	connMap[conn] = msg.Owner
+	fmt.Println(msg)
+	messages <- msg
+}
